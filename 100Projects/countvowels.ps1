ef0823b7852108369665efdf6a4283aa27565822
qqq-jump <qqq-jump@outlook.com>
2013-09-24 23:01:07 +0200

    another project completed

---
+++ 100Projects/countvowels.ps1
@@ -0,0 +1,17 @@
+﻿function count-vowels{
+param(
+    [Parameter(
+        Mandatory = $true,
+        HelpMessage = "Enter String of Text")]
+    [String]$String
+)
+
+    For($i = 0;$i -lt $String.Length;$i ++){
+        
+        If(            ($String[$i] -like "a") -or`            ($String[$i] -like "e") -or`            ($String[$i] -like "i") -or`            ($String[$i] -like "o") -or`            ($String[$i] -like "u")
+        ){
+            $counter ++
+        }
+    }
+    return $counter
+}
